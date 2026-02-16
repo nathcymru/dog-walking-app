452b57cd6574b2abe2a8b872249206d5e5739812
nathcymru <nathcymru@gmail.com>
2026-02-16 12:44:17 +0000

    Add Contact page route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
 import ContactPage from './pages/Contact';
 
-const AppRoutes = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/contact" component={ContactPage} />
-    </Switch>
-  );
-};
+// ...other imports
 
-export default AppRoutes;
+<Route exact path="/contact" component={ContactPage} />
